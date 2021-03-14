4b5ad62f865e2ea4d866b6e8a852b931fe50b596
Владислав <qodize@gmail.com>
2021-03-14 21:29:35 +0500

    organizing files, final version

--- count strings.py
+++
@@ -1,9 +0,0 @@
-with open("strings.txt", encoding='utf-8') as f_in:
-    data = f_in.readlines()
-res = 0
-
-for line in data:
-    if not (line == '\n' or line.startswith('#')):
-       res += 1
-
-print(res)
